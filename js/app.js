5ffcf28275986931fae98df79658097c8e758bc6
RatonRoy <royraton18@gmail.com>
2022-02-17 22:31:15 +0600

    comment the app.js file

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
-// start  selection of the html elements 
-// select all input field 
-const incomeField = document.getElementById('income');
-const foodField = document.getElementById('food');
-const rentField = document.getElementById('rent');
-const colthesField = document.getElementById('colthes');
-const saveField = document.getElementById('save');
-// let x = parseFloat(colthesField.value) + parseFloat(foodField.value) + parseFloat(rentField.value)
-// end of the all input field
-// start  collection of  the input value and convert them number 
-
-const saveFieldValue = saveField.value;
-// end of  collect the input value and convert them number 
-// select the buttons 
-
-// end of the selection button
-// totall  expenses balance element select 
-//remain balance after expense element select 
-
-// saving amount element select that depend on the remaining balance 
-
-// Remaining Balace element select that depends on the saving and balance 
-const remainingBalace = document.getElementById('remaining-balace');
+/* // here every thing is code hard not decelered function 
 document.getElementById('calculation').addEventListener('click', function () {
 	const incomeField = document.getElementById('income');
 	const foodField = document.getElementById('food');
@@ -52,4 +30,4 @@ document.getElementById('saving').addEventListener('click', function () {
 	savingText.innerText = savingAmount;
 	const remainingBalaceElement = document.getElementById('remaining-balace'); 
 	remainingBalaceElement.innerText = parseFloat(remainingBalace) - savingAmount;
-})
+}) */
