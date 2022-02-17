0613e98e95bbc31936503eaa92741271b67ab95f
RatonRoy <royraton18@gmail.com>
2022-02-17 20:17:20 +0600

    complete three error message

--- js/money-function.js
+++ js/money-function.js
@@ -1,20 +1,54 @@
 function getInputValue(inputId) {
 	const inputField = document.getElementById(inputId);
 	const inputText = inputField.value;
+	/* if (inputText.match(/[a-z]/i)) {
+		console.log('is latter');
+		// break;
+	} */
 	const inputAmount = parseFloat(inputText);
-	// inputField.value = ' ';
 	return inputAmount;
 }
 function updateAmount(elementId, amount) {
 	const updateElement = document.getElementById(elementId); 
 	updateElement.innerText = amount;
 }
+// get only input value 
+function inputValue(idName) {
+	const inputElement = document.getElementById(idName);
+	const inputElementValue = inputElement.value;
+	return inputElementValue;
+}
 document.getElementById('calculation').addEventListener('click', function () {
+	// just collect the input value 
+	const incomeElementValue = inputValue('income');
+	const foodElementValue = inputValue('food');
+	const rentElementValue = inputValue('rent');
+	const clothesElementValue = inputValue('clothes'); 
+	// this is the error message if the input value becomes less the 0 
+	if (incomeElementValue < 0 || foodElementValue < 0 || rentElementValue < 0 || clothesElementValue < 0) {
+		const ontiNumberElemnt = document.getElementById('onti-number');
+		ontiNumberElemnt.style.display = 'block';
+		return 0;
+	}
+	// this is the error message if the input characters are letter 
+	if (incomeElementValue.match(/[a-z]/i) || foodElementValue.match(/[a-z]/i) || rentElementValue.match(/[a-z]/i) || clothesElementValue.match(/[a-z]/i)) {
+		const ontiTextElement = document.getElementById('noti-text');
+		ontiTextElement.style.display = 'block';
+		return 0;
+	}
+	 
+	// other content 
 	const incomeAmount = getInputValue('income');
 	const foodAmount = getInputValue('food');
 	const rentAmount = getInputValue('rent');
 	const clothesAmount = getInputValue('clothes');
 	const totallExpenses = foodAmount + rentAmount + clothesAmount;
+	// this is the error message if expenses become bigger than income 
+	if (totallExpenses > incomeAmount) {
+		const claculationNoti = document.getElementById('claculation-noti');
+		claculationNoti.style.display = 'block';
+		return 0;
+	}
 	const balanceAfterExpenses = incomeAmount - totallExpenses;
 	updateAmount('totall-expenses', totallExpenses);
 	updateAmount('balance', balanceAfterExpenses);
